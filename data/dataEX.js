fecee552afe916629f43dd3702261a5bd23d0afb
jafet <20220188@ricaldone.edu.sv>
2024-05-14 14:08:15 -0600

    Trabajo

---
+++ data/dataEX.js
@@ -0,0 +1,24 @@
+const dataEX = [
+    {
+      id: '1',
+      title: '#e2231a',
+      src: require('../src/img/img1.png'),
+    },
+    {
+      id: '2',
+      title: '#FFD300',
+      src: require('../src/img/img2.png'),
+    },
+    {
+      id: '3',
+      title: '#87CEEB',
+      src: require('../src/img/img3.png'),
+    },
+    {
+      id: '4',
+      title: '#87CEEB',
+      src: require('../src/img/img3.png'),
+    }
+  ];
+ 
+  export default dataEX;
